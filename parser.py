7c4df91255a4c6c2b2335e0e8cacb1be52ad1b95
Riquelme Jatay <riquelmejatay@gmail.com>
2026-03-06 17:47:02 -0300

    algumas alterções no parser

--- parser.py
+++ parser.py
@@ -1,10 +1,10 @@
 import sys
 from scanner import Scanner, Token
 
-class ParserError(Exception):
+class parserError(Exception):
     pass
 
-class Parser:
+class parser:
     def __init__(self, Token):
         self.Token = Token
         self.position = 0
@@ -12,7 +12,7 @@ class Parser:
 
     def error(self, message):
         linha = self.current_token.line if self.current_token else "EOF"
-        raise ParserError(f"Erro de sintaxe na linha {linha}: {message}")
+        raise parserError(f"Erro de sintaxe na linha {linha}: {message}")
 
     def advance(self):
         self.position += 1
@@ -93,24 +93,122 @@ class Parser:
         self.match("LPAREN")
 
         if self.current_token.type != "RPAREN":
-            self.parse_formal_params()   
+            self.parser_formal_params()   
         self.match("RPAREN")
         self.match("COLON")
-        self.parse_type()
-        self.parse_block()
+        self.parser_type()
+        self.parser_block()
 
 # <formal-params> => <formal-param> <formal-param_list>
 # <formal-param_list> => "," <formal-param> <formal-param_list> | e
 # <formal-param> => <identifier> ":" <type>
-    def parse_formal_params(self):
-        self.parse_formal_param()
+    def parser_formal_params(self):
+        self.parser_formal_param()
         
         while self.current_token is not None and self.current_token.type == "COMMA":
             
             self.match("COMMA")          
-            self.parse_formal_param()    
+            self.parser_formal_param()    
 
-    def parse_formal_param(self):
+    def parser_formal_param(self):
         self.match("IDENTIFIER")
         self.match("COLON")
-        self.parse_type()
+        self.parser_type()
+
+# <while-statement> => "while" "(" <expression> ")" <block>
+
+    def parser_while_statement(self):
+        self.match("KEYWORD", "while")
+        self.match("LPAREN")
+
+        if self.current_token.type != "RPAREN":
+            self.parser_expression() 
+        self.match("RPAREN")
+        self.parser_block()
+
+# <if-statement> => "if" "(" <expression> ")" <block> <opt_else>
+# <opt_else> => "else" <block> | e
+
+    def parser_if_statement(self):
+        self.match("KEYWORD", "if")
+        self.match("LPAREN")
+
+        if self.current_token.type != "RPAREN":
+            self.parser_expression() 
+        self.match("RPAREN")
+        self.parser_block()
+        self.parser_opt_else()
+
+    def parser_opt_else(self):
+        self.match("KEYWORD", "else")
+        self.parser_block()
+
+
+
+# <return-statement> => "return" <expression>
+    def parser_return_statement(self):
+        self.match("KEYWORD", "return")
+        self.parser_expression()
+
+#<print-statement> => "print" <string-literal>
+    def parser_print_statement(self):
+        self.match("KEYWORD", "print")
+        self.parser_string_literal()
+
+# <type> => "int" | "real" | "bool" | "void"
+    def parser_type(self):
+
+# <variable-decl> => "var" <identifier> ":" <type> <expression>
+# <assignment> => "set" <identifier> "=" <expression>
+
+# <expression> => <simple-expression> <relational_list>
+#<relational_list> => <relational-op> <simple-expression> <relational_list> | e
+
+#<simple-expression> => <term> <additive_list>
+#<additive_list> => <additive-op> <term> <additive_list> | e
+
+#<term> => <factor> <multiplicative_list>
+#<multiplicative_list> => <multiplicative-op> <factor> <multiplicative_list> | e
+
+#<factor> => <literal> | <identifier> | <function-call> | <sub-expression> | <unary>
+
+#<unary> => <unary_op> <expression_list>
+#<unary_op> => "+" | "-" | "not"
+#<expression_list> => <expression> <expression_list> | e
+
+#<sub-expression> => "(" <expression> ")"
+
+#<function-call> => <identifier> "(" <opt_actual_params> ")"
+
+#<actual-params> => <expression> <actual-param_list>
+#<opt_actual_params> => <actual-params> | e
+
+#<actual-param_list> => "," <expression> <actual-param_list> | e
+
+#<relational-op> => "<" | ">" | "==" | "!=" | "<=" | ">="
+
+#     def parser_relational_op(self):
+#         self.match("PLUS") 
+        
+# #<additive-op> => "+" | "-" | "or"
+#<multiplicative-op> => "*" | "/" | "and"
+
+#<identifier> => <id_start> <id_rest_list>
+#<id_start> => "_" | <letter>
+#<id_rest_list> => <id_rest> <id_rest_list> | e
+#<id_rest> => "_" | <letter> | <digit>
+
+#<digit> => "0" | ... | "9"
+#<letter> => "a" | ... | "z" | "A" | ... | "Z"
+
+#<literal> => <integer-literal> | <real-literal> | "true" | "false"
+
+#<integer-literal> => <digit> <digit_list>
+#<digit_list> => <digit> <digit_list> | e
+
+#<real-literal> => <integer-literal> "." <integer-literal>
+
+#<string-literal> => " " " <string_char_list> " " "
+#<string_char_list> => <string_char> <string_char_list> | e
+#<string_char> => <letter> | <digit> | <symbol>
+#<symbol> => <any-char-except-quote>
